41fe47dac0b5cf7211a43b073c4c323360f7d327
Samuel Karani Mbaabu <samuel.karani@berkeley.edu>
2018-06-30 04:19:19 -0700

    removed options and content scripts

--- src/content_scripts/content.js
+++
@@ -1,7 +0,0 @@
-console.log("from the content scripts");
-
-const anchor = document.createElement("div");
-anchor.id = "rcr-anchor";
-anchor.appendChild(document.createTextNode("hello world"));
-
-// document.body.insertBefore(anchor, document.body.childNodes[0]);
