d17b2e999457bfc4d07daa98d30cda5661a7a393
Samuel Karani Mbaabu <samuel.karani@berkeley.edu>
2018-07-02 15:53:51 -0700

    fixed context menu bug

--- src/background/contextMenu.js
+++ src/background/contextMenu.js
@@ -5,12 +5,15 @@ export default function setupContextMenus({ store, addTodo, ID_GENERATOR }) {
     title: "Add as Todo",
     contexts: ["selection"]
   };
-  chrome.contextMenus.create(menuItem);
-  chrome.contextMenus.onClicked.addListener(function(selection) {
-    if (selection.menuItemId === ID && selection.selectionText) {
-      store.dispatch(
-        addTodo({ id: ID_GENERATOR(), title: selection.selectionText })
-      );
-    }
+
+  chrome.contextMenus.removeAll(function() {
+    chrome.contextMenus.create(menuItem);
+    chrome.contextMenus.onClicked.addListener(function(selection) {
+      if (selection.menuItemId === ID && selection.selectionText) {
+        store.dispatch(
+          addTodo({ id: ID_GENERATOR(), title: selection.selectionText })
+        );
+      }
+    });
   });
 }
